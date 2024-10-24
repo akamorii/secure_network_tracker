d16bca856f85374f14b672c7b485ff1430a41071
akamori <akamorrii@gmail.com>
2024-10-24 14:33:43 +0300

    feat(webapp): добавлено немного фронтенда а также файл для создания бд

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False )
+    password = db.Column(db.String, nullable=False)
+    
+    def __repr__(self):
+        return '<article %r>' %self.id
 
 @app.route('/')
 def index():
@@ -10,13 +20,17 @@ def index():
 
 @app.route('/about')
 def about_page():
-    return 'about'
+    return render_template('about.html')
 
 
 @app.route('/user/<string:name>/<int:iden>')
 def user_info(name, iden):
     a = f'user_name: {name} user_id: {str(iden)}'
     return a
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
